58059a85879c6484f24d61d0de56910cc1caf70c
Niaz Baharudeen <tuk05348@temple.edu>
2024-06-16 20:30:16 -0400

    Again adapting from the Medium code, implementation of the API request using Retrofit and OkHttp to log the API response and parse the json. Implemented using a coroutine in the MainActivity. Dependency for HttpLoggingInterceptor added. Code as of now is work in progress, Items do not map to the list properly because of null names.

---
+++ app/src/main/java/edu/temple/fetchapp/ItemListImpl.kt
@@ -0,0 +1,50 @@
+package edu.temple.fetchapp
+
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.awaitResponse
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ItemListImpl: ItemList {
+
+    private val baseUrl = "https://fetch-hiring.s3.amazonaws.com/"
+
+    private val itemAPI: ItemAPI
+
+    init {
+        val mHttpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val mOkHttpClient = OkHttpClient
+            .Builder()
+            .addInterceptor(mHttpLoggingInterceptor)
+            .build()
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(mOkHttpClient)
+            .build()
+
+        itemAPI = retrofit.create(ItemAPI::class.java)
+    }
+
+    override suspend fun getItems(): List<Item> {
+        return try {
+            val response = itemAPI.getItems().awaitResponse()
+            if (response.isSuccessful) {
+                val items = response.body() ?: emptyList()
+                items.mapNotNull {
+                    it.toModel()
+                }
+            } else {
+                emptyList()
+            }
+        } catch (exception: Exception) {
+            Log.d("E", exception.toString())
+            emptyList()
+        }
+    }
+}
--- app/src/main/java/edu/temple/fetchapp/MainActivity.kt
+++ app/src/main/java/edu/temple/fetchapp/MainActivity.kt
@@ -2,10 +2,21 @@ package edu.temple.fetchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
+        scope.launch {
+            Log.d("ItemList", ItemListImpl().getItems().toString())
+        }
     }
 }
