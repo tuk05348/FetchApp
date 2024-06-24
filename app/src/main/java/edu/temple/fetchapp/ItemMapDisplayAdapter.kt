00d2d3f2fd25dd97f8a92ad6f4509a6ff87a55d2
Niaz Baharudeen <tuk05348@temple.edu>
2024-06-23 22:49:05 -0400

    Added bare skeleton for implementing recycler view for displaying the items grouped by ID, current idea is to use nested recycler views.

---
+++ app/src/main/java/edu/temple/fetchapp/ItemMapDisplayAdapter.kt
@@ -0,0 +1,23 @@
+package edu.temple.fetchapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemMapDisplayAdapter (_itemMap : Map<Int, List<String>>) : RecyclerView.Adapter<ItemMapDisplayAdapter.ItemMapViewHolder>() {
+    inner class ItemMapViewHolder(layout: View) : RecyclerView.ViewHolder(layout) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemMapViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemMapViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
