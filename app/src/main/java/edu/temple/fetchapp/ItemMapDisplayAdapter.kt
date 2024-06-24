1a2ac33f76602856a5bf70c9065d355acb55ed70
Niaz Baharudeen <tuk05348@temple.edu>
2024-06-24 13:27:04 -0400

    Fixed error in item sorting, it previously sorted items lexicographically rather than numerically, extracted number from string and sorted by that now. Also filled in recycler view adapter, for now no nesting, basic display of list ID and list of items.

--- app/src/main/java/edu/temple/fetchapp/ItemMapDisplayAdapter.kt
+++ app/src/main/java/edu/temple/fetchapp/ItemMapDisplayAdapter.kt
@@ -1,23 +1,30 @@
 package edu.temple.fetchapp
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemMapDisplayAdapter (_itemMap : Map<Int, List<String>>) : RecyclerView.Adapter<ItemMapDisplayAdapter.ItemMapViewHolder>() {
-    inner class ItemMapViewHolder(layout: View) : RecyclerView.ViewHolder(layout) {
 
+    private val itemMap = _itemMap
+    inner class ItemMapViewHolder(layout: View) : RecyclerView.ViewHolder(layout) {
+        val listIdTextView = layout.findViewById<TextView>(R.id.listIdTextView)
+        val nameListTextView = layout.findViewById<TextView>(R.id.nameListTextView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemMapViewHolder {
-        TODO("Not yet implemented")
+        return ItemMapViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_itemmap, parent, false)
+        )
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = itemMap.size
 
     override fun onBindViewHolder(holder: ItemMapViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val key = itemMap.keys.elementAt(position)
+        holder.listIdTextView.text = key.toString()
+        holder.nameListTextView.text = itemMap[key].toString()
     }
 }
--- app/src/main/java/edu/temple/fetchapp/MainActivity.kt
+++ app/src/main/java/edu/temple/fetchapp/MainActivity.kt
@@ -3,23 +3,32 @@ package edu.temple.fetchapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         val scope = CoroutineScope(Job() + Dispatchers.Default)
 
         scope.launch {
             val itemMap: Map<Int, List<String>> = ItemListImpl()
-                .getItems().sortedWith(compareBy({it.listId}, {it.name}))
+                .getItems().sortedWith(compareBy({it.listId}, { it.name.filter { name -> name.isDigit() }.toInt() }))
                 .groupBy ({it.listId}, {it.name} )
-            Log.d("map", itemMap.toString())
+            withContext(Dispatchers.Main) {
+                recyclerView.adapter = ItemMapDisplayAdapter(itemMap)
+                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            }
         }
     }
 }
